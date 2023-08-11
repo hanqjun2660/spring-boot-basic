df3ef95b1ea1a7d22088b290ad35ebe8f1b0f81e
KANG\b2000 <hanqjun2660@gmail.com>
2023-08-11 23:52:18 +0900

    navbar 추가

---
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,14 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMettYou(Model model) {
+        model.addAttribute("username", "홍길동");
+        return "greetings";
+    }
+}
