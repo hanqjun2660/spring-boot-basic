0c7e237faebabbd1e803236826d836adcbc1b1de
KANG\b2000 <hanqjun2660@gmail.com>
2023-08-14 16:06:08 +0900

    게시글 수정요청 코드 개선

--- firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -44,28 +44,14 @@ public class ArticleApiController {
         log.info(created.toString());
         return (created != null) ? ResponseEntity.status(HttpStatus.OK).body(created) : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
-//
-//    @PatchMapping("/api/articles/{id}")
-//    public ResponseEntity<Article> update(@PathVariable long id, @RequestBody ArticleForm dto) {
-//        // DTO -> Entity 변환
-//        Article article = dto.toEntity();
-//        log.info("id: {}, article : {},", id, article.toString());
-//
-//        // 해당 id의 게시글 조회
-//        Article target = articleRepository.findById(id).orElse(null);
-//
-//        // 잘못된 요청 막기
-//        if(target == null || id != article.getId()) {
-//            log.info("잘못된 요청! id : {}, article {}", id, article.toString());
-//            // ResponseEntity : REST API의 응답을 위해 사용 -> 상태코드, 헤더, 본문을 되돌려보낼 수 있다.
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-//        }
-//
-//        // 업데이트 및 정상 응답 하기
-//        target.patch(article);
-//        Article updated = articleRepository.save(target);
-//        return ResponseEntity.status(HttpStatus.OK).body(updated);
-//    }
+
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable long id, @RequestBody ArticleForm dto) {
+
+        Article update = articleService.update(id, dto);
+
+        return (update != null) ? ResponseEntity.status(HttpStatus.OK).body(update) : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 //
 //    @DeleteMapping("/api/articles/{id}")
 //    public ResponseEntity<Article> delete(@PathVariable long id) {
--- firstproject/src/main/java/com/example/firstproject/service/ArticleService.java
+++ firstproject/src/main/java/com/example/firstproject/service/ArticleService.java
@@ -5,6 +5,7 @@ import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 
@@ -33,5 +34,24 @@ public class ArticleService {
             return null;
         }
         return articleRepository.save(article);
+    }
+
+    public Article update(long id, ArticleForm dto) {
+
+        // DTO -> Article 변환
+        Article article = dto.toEntity();
+
+        // 원래 게시글이 존재하는지 확인하기 위해 조회
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 게시글이 없거나, 입력받은 id와 DTO -> Article로 변환된 객체의 id가 다른경우 비정상 요청으로 보고 null 반환
+        if(target == null || ObjectUtils.isEmpty(target) || id != article.getId()) {
+            return null;
+        }
+
+        // 문제가 없는 경우 update / 원래 게시글에 일부 수정만 하도록 patch 메서드를 정의하여 사용
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return updated;
     }
 }
